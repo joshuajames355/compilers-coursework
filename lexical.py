7c545f71d9790b4f26fe5614e40dabd44a4d565a
Josh <joshuajames355@gmail.com>
2020-03-06 17:28:48 +0000

    lexical analysis.

---
+++ lexical.py
@@ -0,0 +1,102 @@
+import string
+
+VARIABLE = 0
+CONSTANT = 1
+PREDICATE = 2
+EQUALITY = 3
+CONNECTIVE = 4
+QUANTIFIER = 5
+BRACKET_START = 6 
+BRACKET_END = 7
+COMMA = 8
+
+class Token:
+    tokenClass = -1 #one of the values above
+    name = "" #the name 
+    attributes = -1 #for predicate, the arity
+
+    def __init__(self, tokenClass, name, attributes):
+        self.tokenClass = tokenClass
+        self.name = name
+        self.attributes = attributes
+
+    def __str__(self):
+        return "Class: {} - Name: {} Attributes: {}".format(self.tokenClass, self.name, self.attributes)
+
+def tokensFromTokensImport(tokensImport):
+    out = []
+    for each in tokensImport.variables:
+        out += [Token(VARIABLE, each, -1)]
+    for each in tokensImport.constants:
+        out += [Token(CONSTANT, each, -1)]
+    for each in tokensImport.predicates:
+        out += [Token(PREDICATE, each.name, each.arity)]
+    for each in tokensImport.equality:
+        out += [Token(EQUALITY, each, -1)]
+    for each in tokensImport.connectives:
+        out += [Token(CONNECTIVE, each, -1)]
+    for each in tokensImport.quantifiers:
+        out += [Token(QUANTIFIER, each, -1)]
+    return out
+
+#tokens - a list of strings, representing all possible tokens
+#source - string
+def lexical(tokens, source):
+    MAX_TOKEN_LENGTH = max(map(len, tokens.getTokenList()))
+    TOKENS_BY_SIZE = [list(filter(lambda x : len(x.name) == size, tokensFromTokensImport(tokens))) for size in range(MAX_TOKEN_LENGTH + 1)]
+
+    output = []
+
+    start = 0
+    current = 0
+    while current < len(source):
+        currentChar = source[current]
+        currentToken = source[start:current]
+        currentLength = current - start
+
+        mustAddCurrent = False #if matches have been found, either in the current char, or start:current (not including current)
+        mustAddPrevious = False
+
+        if currentChar in ["(", ")", ","]: #handle these seperatley, as all other tokens are seperated by whitespace (or these chars)
+            mustAddCurrent = True
+            mustAddPrevious = True
+        if currentChar in string.whitespace:
+            if start == current: #strip leading whitespace
+                start += 1
+                current += 1
+                continue
+            mustAddPrevious = True
+        #ensure final char in source code is handled correctly
+        if current == len(source) - 1 and not mustAddCurrent:
+            currentToken = source[start:current+1]
+            currentLength = current - start +1
+            mustAddCurrent = True
+
+        if currentLength > MAX_TOKEN_LENGTH:
+            raise ValueError("Invalid token: {} in formula at position: {}".format(currentToken, start))
+
+        #match tokens
+        if mustAddPrevious and currentLength > 0:
+            match = False
+            for token in TOKENS_BY_SIZE[currentLength]:
+                if currentToken == token.name:
+                    match = token
+                    break
+            if not match:
+                raise ValueError("Invalid token: {} in formula at position: {}".format(currentToken, start))
+
+            output += [match]
+
+            start = current + 1
+        if mustAddCurrent:
+            if currentChar == "(":
+                output += [Token(BRACKET_START, "(", -1)]
+            if currentChar == ")":
+                output += [Token(BRACKET_END, ")", -1)]
+            if currentChar == ",":
+                output += [Token(COMMA, ",", -1)]
+            start = current + 1
+
+        current += 1
+
+    return output
--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import fileParser
+from lexical import lexical
+
 def main():
     tokens, formula = fileParser.loadFile("example1.txt")
+    test = lexical(tokens, formula)
 
 main()
+
+#Start symbol S - capitals mark non terminals
+#S -> T | (S ^ S) | ... | not S
+#T -> P(y1..yd) | (C = D) | (C = x) | (x = C) | (x = y)     p a predicate of arity d, C,D constants, x,y variables
